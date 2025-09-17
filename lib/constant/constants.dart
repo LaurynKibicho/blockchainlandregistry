db8e17956838490b615e2c4dc774a095aaf86aea
LaurynKibicho <lauryn.kibicho@strathmore.edu>
2025-09-17 17:24:18 +0300

    initial commit

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -5,8 +5,8 @@ String nftStorageApiKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDBkODk0MkFGRDI4QUU0NjMzNjc5MEJjQzU0MzVhZTQ5NzAyNEU5MjMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTcwMzMzOTk0MTAwNiwibmFtZSI6ImxhbmQxIn0.YK9LPFc-Gu5o23WCBgecK4mm8PHxx1Cjc_3Iduq3Y1o';
 
 const String rpcUrl = "http://127.0.0.1:7545";
-  
-const String contractAddress = "0xBafDA704E74c0b2C05442Ef86FfAb5Cb5A583e4B";
+
+const String contractAddress = "0x7f80303f4DF00d76E6f48A16fD81D896d318687B";
 
 const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
 
